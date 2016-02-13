349606506d01343b47644f8f017f013a076ec408
raymondberg <dev@raymondberg.com>
2016-02-12 18:36:55 -0800

    Use sqlite for local development.

--- btray/__init__.py
+++ btray/__init__.py
@@ -13,13 +13,17 @@ app.config.from_pyfile('../configuration.py')
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % (
-    app.config['MYSQL_USERNAME'],
-    app.config['MYSQL_PASSWORD'],
-    app.config['MYSQL_HOST'],
-    app.config['MYSQL_PORT'],
-    app.config['MYSQL_DATABASE']
-)
+if app.config['ENVIRONMENT'] == 'production':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % (
+        app.config['MYSQL_USERNAME'],
+        app.config['MYSQL_PASSWORD'],
+        app.config['MYSQL_HOST'],
+        app.config['MYSQL_PORT'],
+        app.config['MYSQL_DATABASE']
+    )
+elif app.config['ENVIRONMENT'] == 'development':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.sqlite'
+
 
 db = SQLAlchemy(app)
 db.PasswordType = PasswordType
