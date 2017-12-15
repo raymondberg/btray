ab886425daad6c0bb90e7c599e65965dfd851863
carter_caroline <code@getbraintree.com>
2017-12-15 19:32:22 +0000

    Add error handling

--- btray/__init__.py
+++ btray/__init__.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import logging
 
 from flask import Flask
 from flask.ext.login import LoginManager
@@ -7,6 +9,9 @@ from sqlalchemy_utils import PasswordType
 from flask_sqlalchemy import orm
 
 app = Flask(__name__, template_folder='../templates')
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
